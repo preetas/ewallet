48e5b9958f353d006c993264cd07911f52b8aa32
preetas <88144614+preetas@users.noreply.github.com>
2021-07-29 20:44:20 +0530

    Create Admin.java

---
+++ NewOne/src/beans/Admin.java
@@ -0,0 +1,34 @@
+package beans;
+
+public class Admin {
+
+	String email;
+	String password;
+	public Admin() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Admin(String email, String password) {
+		super();
+		this.email = email;
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "Admin [email=" + email + ", password=" + password + "]";
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
